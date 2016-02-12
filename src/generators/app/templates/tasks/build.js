9cd99cdf960713691cde61c96318f056e7a63721
spalger <spalger@users.noreply.github.com>
2016-02-12 13:25:24 -0700

    [tasks] typo

--- src/generators/app/templates/tasks/build.js
+++ src/generators/app/templates/tasks/build.js
@@ -3,7 +3,7 @@
  *
  * Copies files from the source into a zip archive
  * that can be distributed for installation into production
- * kibana instals. The archive includes the non-development
+ * kibana installs. The archive includes the non-development
  * npm dependencies and builds itself using raw files in
  * the source directory so make sure they are clean/up to date.
  *
